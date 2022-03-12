c868ee42b65bfeabedee765b48790a4757f846c8
DLinh <nogamenolife.why@gmail.com>
2022-03-12 19:51:07 +0700

    feat: eslint and prettier install

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
@@ -39,7 +36,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+type User = {
+  name: string;
+  status: string;
+  age: number;
+};
+const user: User = {
+  name: 'Vite',
+  status: '💪',
+  age: 1.5,
+};
+function UserInfo({ name, status, age }: User) {
+  return (
+    <div className="mx-auto bg-rose-300">
+      <p>{name}</p>
+      <p>{status}</p>
+      <p>{age}</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <div>
+    <UserInfo name={user.name} status={user.status} age={user.age} />
+  </div>,
+  document.querySelector('#root')
+);
